bdccc70332c535d298dceba5e59dbcdcf7ee353e
abdullahakintobi <akintobiabdullah@gmail.com>
2025-06-19 20:56:23 +0100

    feat: Add first C program to print out some messages

---
+++ c/first_program.c
@@ -0,0 +1,10 @@
+// My first C program to print out a message
+#include <stdio.h>
+
+int main() {
+    printf("This is my first C program. \n");
+    printf("Looking forward to writing more C programs!\n");
+    printf("Cheers! :)");
+
+    return 0;
+}
